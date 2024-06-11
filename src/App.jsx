131ba67a6977f29dbc417c51b76100f87808013d
Afaq Ahmad Rajpoot <ranag55db@gmail.com>
2024-06-11 10:05:18 +0500

    Header Styling in process

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
+import "./utils.css";
+import "./App.css"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home/Home"
 import Cart from './pages/Cart/Cart';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="App container">
         <Header />
         <Routes>
-          <Route path="/" component={<Home/>} />
-          <Route path="/cart" component={<Cart/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
         <Footer />
       </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,38 @@
 import "./Header.css"
 const Header = () => {
   return (
-    <div className="header">
+    <div className="">
+      <div className="header-top">
+
+
+        <div className="drop-down">
+            <select name="" id="">
+              <option value="CURRENCY">CURRENCY</option>
+              <option value="€ EUR">€ EUR</option>
+              <option value="£ Pound Sterling">£ Pound Sterling</option>
+              <option value="$ Us Dollar">$ US Dollar</option>
+            </select>
+
+
+            <select name="" id="">
+              <option value="LANGUAGE">LANGUAGE</option>
+              <option value="English"> English</option>;
+              <option value="French">French</option>
+            </select>
+        </div>
+        <div>
+          <ul>
+            <li>My Account</li>
+            <li>Wishlist</li>
+            <li>Register or Sign in</li>
+            <li>Checkout</li>
+          </ul>
+        </div>
+        
+
+
+      </div>
+      <hr />
         <div className="logo"><img src="/logo.png" alt="" /></div>
         <ul className="header-items">
             <li>Home</li>
